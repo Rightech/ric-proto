9fc23d29e9b66d2fc1d87716c8fb8482a42c9a08
inn0kenty <ilebedev@mm.st>
2020-06-09 14:17:56 +0300

    Fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,13 @@ ln -s /node/node_modules ric-proto/node_modules
 
 cd ric-proto
 
+# because ./compile load .env file we need move it
+mv .env .env.back || true
+
 ./compile.sh
+
+# move .env back
+mv .env.back .env || true
 
 rm node_modules
 
