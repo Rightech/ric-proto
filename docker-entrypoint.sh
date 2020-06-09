b766a5065c4d5e53c7941e7eb2c294c089b9def2
inn0kenty <ilebedev@mm.st>
2020-06-09 14:25:39 +0300

    Fix compile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-rm -rf ric-proto/node_modules
+rm -rf /ric-proto/node_modules
 
-ln -s /node/node_modules ric-proto/node_modules
+ln -s /node/node_modules /ric-proto/node_modules
 
 cd ric-proto
 
