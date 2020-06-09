33cfc64ec4ea2acf5186df65b598205ecf342e52
inn0kenty <ilebedev@mm.st>
2020-06-09 16:00:07 +0300

    Fix compile

--- compile.sh
+++ compile.sh
@@ -4,8 +4,15 @@ if [[ -f .env ]]; then
     source .env
 fi
 
+rm -rf cpp
+rm -rf go
+rm -rf dts
+
+mkdir dts
+
 prototool format -w
 prototool generate
+
 protoc-go-inject-tag -input=go/ric-tasks/rictasks.pb.go -XXX_skip=bson
 protoc-go-inject-tag -input=go/ric-bots/ricbots.pb.go -XXX_skip=bson
 
