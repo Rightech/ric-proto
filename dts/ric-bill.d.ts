e8b59fb9f6f6fc845e716df44b87e0e4b4613125
Oleg Prohazko <prohazko@rightech.io>
2020-06-29 13:28:35 +0000

    Commit from GitHub Actions

---
+++ dts/ric-bill.d.ts
@@ -0,0 +1,46 @@
+export interface Billing {
+  SetupAccount(request: SetupRequest): Promise<SetupResponse>;
+  VerifyAccount(request: SetupRequest): Promise<SetupResponse>;
+  CloseAccount(request: SetupRequest): Promise<SetupResponse>;
+  CreateSubscription(request: SubscriptionRequest): Promise<SubscriptionResponse>;
+  UpdateSubscription(request: SubscriptionRequest): Promise<SubscriptionResponse>;
+  CancelSubscription(request: SubscriptionRequest): Promise<SubscriptionResponse>;
+  ResumeSubscription(request: SubscriptionRequest): Promise<SubscriptionResponse>;
+  ActivateSubscription(request: SubscriptionRequest): Promise<SubscriptionResponse>;
+  DeactivateSubscription(request: SubscriptionRequest): Promise<SubscriptionResponse>;
+}
+
+export interface UserContext {
+  groupId?: string;
+  userId?: string;
+  spanId?: string;
+}
+
+export interface SetupRequest {
+  ctx?: UserContext;
+}
+
+export interface SetupResponse {
+  bindingId?: string;
+  widgetParams?: string;
+}
+
+export interface DryRun {
+  amount?: string;
+  currency?: string;
+  nextPay?: number;
+  messages?: string[];
+}
+
+export interface SubscriptionRequest {
+  ctx?: UserContext;
+  licenseId?: string;
+  dryRun?: boolean;
+  params?: string;
+}
+
+export interface SubscriptionResponse {
+  paymentId?: string;
+  jobId?: string;
+  dryRun?: DryRun;
+}
